5960a5ec43540f322444a4ecdab245ae37bc673b
davidita118 <david.isaac.iker@hotmail.com>
2020-02-24 21:58:22 -0600

    first commit

--- php-functions/getNews.php
+++ php-functions/getNews.php
@@ -9,43 +9,46 @@ $feed = new SimplePie();
 if()
 
 function existURL($url) {
-    $file = fopen('urls.txt', 'r');
+    $file = fopen('urls.txt', 'a+');
     
     while($line = fgets($file)) {
-        if ($url == $line) {
+        if ($url === $line) {
             return true;
         }
     }
+    fputs($file, $url);
     fclose($file);
     
     return false;
 }
 
-function matchNews($title) {
+function matchNews($word) {
     require './php-functions/db.php';
     
-    $query = $connection->prepare('SELECT * FROM notice MATCH title = :title');
-    $query->bindParam(':title', $title);
+    $query = $connection->prepare('SELECT * FROM notice MATCH word = :word');
+    $query->bindParam(':word', $word);
     $query->execute();
     $results = $query->fetch(PDO::FETCH_ASSOC);
     
-    echo json_encode(new News($results['title'], $results['author'], $results['date'], $results['description']));
-    
-    $connection
+    echo json_encode(new News($results['title'], $results['author'], $results['date'], $results['description'], $results['content']));
 }
 
 function insertNews($news) {
+    require './php-functions/db.php';
     
+    $query = $connection->prepare('INSERT * FROM notice WHERE title = :title, author = :author, date = :date, description = :description, content = :content');
+    $query->bindParam(':title', $news->getTitle());
+    $query->bindParam(':author', $news->getAuthor());
+    $query->bindParam(':date', $news->getDate());
+    $query->bindParam(':description', $news->getDescription());
+    $query->bindParam(':content', $news->getContent());
+    $query->execute();
 }
 
 $feed->set_feed_url($url);
 
 $feed->init();
 $item = $feed->get_item(0);
-
-
-
-
 $link = $item->get_link();
 $title = $item->get_title();
 $author = $item->get_author()->get_name();
