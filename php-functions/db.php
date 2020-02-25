12033dca8dff3e4c00d54255c1bba0b8640e5e3e
davidita118 <david.isaac.iker@hotmail.com>
2020-02-24 19:26:08 -0600

    db.php commit

--- php-functions/db.php
+++ php-functions/db.php
@@ -1,2 +1,19 @@
 <?php
 
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'NewsDB';
+$connection = null;
+
+function getConnection() {
+    try {
+        $connection = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+    } catch (Exception $ex) {
+        die('Connection failed:'.$ex->getMessage());
+    return $connection;
+}
+
+function closeConnection() {
+    $connection->closeConnection();
+}
