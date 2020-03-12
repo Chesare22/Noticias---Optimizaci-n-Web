578d007ba41a1ea206aee588816c5fcac88b3115
Chesare22 <Chesare22@github.com>
2020-03-12 12:10:29 -0600

    [FEAT] View for interactable elements

--- js/index.js
+++ js/index.js
@@ -1,24 +1,12 @@
-const url = 'http://www.reforma.com/rss/portada.xml';
+/* global $ */
 
-function getNews (addNews) {
-  const xmlhttp = new XMLHttpRequest();
-  xmlhttp.open('get', `php-functions/getNews.php?url=${url}`, true);
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      addNews(JSON.parse(this.responseText));
-    }
-  };
-  xmlhttp.send();
+function autoGrow (element) {
+  element.style.height = '30px';
+  element.style.height = `${element.scrollHeight - 4}px`;
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  getNews(news => {
-    const link = document.getElementById('title');
-    link.innerText = news.title;
-    link.href = news.link;
-    document.getElementById('author').innerText = news.author;
-    document.getElementById('date').innerText = news.date;
-    document.getElementById('description').innerText = news.description;
-    document.getElementById('content').innerText = news.content;
+window.onload(() => {
+  $('#urls').on('input', function () {
+    autoGrow(this);
   });
 });
