1d70084f32eea25fc931b7a6b62d45a8809fd1d0
Chesare22 <Chesare22@github.com>
2020-05-01 18:43:34 -0500

    feat: Show search results

--- js/index.js
+++ js/index.js
@@ -31,8 +31,37 @@ window.onload = () => {
     const word = $('#search').val();
     $(this).addClass('loading')
     $.ajax(`${backendUrl}?word=${word}`, {
-      success (response) {
-        console.log(response);
+      success (pages) {
+        const dateFormatter = new Intl.DateTimeFormat('es-MX', { year: 'numeric', month: 'long', day: 'numeric' });
+        const elements = pages.map(page => {
+          const linkAndDescription = document.createElement('div');
+          linkAndDescription.className = 'link-and-description';
+
+          const link = document.createElement('h3');
+          $(link).append(
+            $(document.createElement('a'))
+              .attr('href',page.URL)
+              .attr('target','_blank')
+              .text(page.URL)
+          );
+          linkAndDescription.appendChild(link);
+
+          const date = document.createElement('div');
+          date.className = 'date';
+          date.innerText = dateFormatter.format(new Date(page.Date));
+          linkAndDescription.appendChild(date);
+
+          const description = document.createElement('div');
+          description.className = 'description';
+          description.innerText = page.Text;
+          linkAndDescription.appendChild(description);
+
+          return linkAndDescription;
+        })
+
+        $('#results')
+          .empty()
+          .append(...elements)
       },
       error (error) {
         console.log(error);
